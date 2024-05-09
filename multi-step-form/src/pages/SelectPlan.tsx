0598f31f73b470f957e79f01a627af8fc0f7870d
ellie <elliekoooo@gmail.com>
2024-05-10 01:04:45 +0900

    add page

--- multi-step-form/src/pages/SelectPlan.tsx
+++ multi-step-form/src/pages/SelectPlan.tsx
@@ -39,49 +39,59 @@ const array2 = [
 
 const SelectPlan:any = (array: {}[]) => {
     return (
-    <form className='my-6 pr-6 mr-6'>
-        <div className='field'>
-            <div className='columns is-centered'>
-                {array2.map(a => {
-                  return <div className='column is-3 mx-1'>
-                        <div className='box is-rounded'>
-                            <figure className='image is-32x32 mb-6 pb-6'>
-                                <img className='is-rounded' src={"../images/icon-"+a.name+".svg"} />
-                            </figure>
-                            <div className='has-text-weight-semibold has-text-link'>
-                                {a.name}
+        <div className='container'>
+            <div className='is-size-3 has-text-weight-bold'>
+                Select your plan      
+            </div>
+            <span>You have the opinion of monthly or yearly billing</span>
+            <form className='my-6 pr-6 mr-6'>
+                <div className='field'>
+                    <div className='columns is-centered'>
+                        {array2.map(a => {
+                        return <div className='column is-3 mx-1'>
+                                <div className='box is-rounded'>
+                                    <figure className='image is-32x32 mb-6 pb-6'>
+                                        <img className='is-rounded' src={"../images/icon-"+a.name+".svg"} />
+                                    </figure>
+                                    <div className='has-text-weight-semibold has-text-link'>
+                                        {a.name}
+                                    </div>
+                                    <span className='is-size-7 has-text-grey'>
+                                        ${a.plan['monthly']['price']}/mo
+                                    </span>
+                                </div>
                             </div>
-                            <span className='is-size-7 has-text-grey'>
-                                ${a.plan['monthly']['price']}/mo
-                            </span>
+                        })}
+                    </div>
+                </div>
+                <div className='field my-6'>
+                    <div className='columns is-centered has-text-link has-text-weight-bold'>
+                        <div className='column is-size-7 is-flex is-justify-content-center is-align-items-center py-0 is-9 has-background-info-light'>
+                            <span className='mx-3'>Monthly</span>
+                            <input id='planner' type='checkbox' name='planner' className='switch is-rounded is-small' checked></input>
+                            <label htmlFor="planner"></label>
+                            <span className='mx-3'>Yearly</span>
                         </div>
                     </div>
-                })}
-            </div>
-        </div>
-        <div className='field my-6'>
-            <div className='columns is-centered is-vcentered has-text-link has-text-weight-bold has-text-centered'>
-                    <span className='mx-5'>Monthly</span>
-                    <input id='planner' type='checkbox' name='planner' className='switch is-rounded' checked></input>
-                    <label htmlFor="planner"></label>
-                    <span className='mx-5'>Yearly</span>
-            </div>
-        </div>
-        <div className='field'>
-            <div className='columns'>
-                <div className='control'>
-                    <button className='button is-medium is-link'>
-                        <span className='is-size-6'>Next step</span>
-                    </button>
                 </div>
-                <div className='control'>
-                    <button className='button is-medium is-link'>
-                    <span className='is-size-6'>Next step</span>
-                    </button>
+                <div className='field'>
+                    <div className='columns is-centered'>
+                        <div className='column px-0 is-9 is-flex is-justify-content-space-between'>
+                            <div className='control'>
+                                <button className='button is-medium is-link'>
+                                    <span className='is-size-6'>Go back</span>
+                                </button>
+                            </div>
+                            <div className='control'>
+                                <button className='button is-medium is-link'>
+                                    <span className='is-size-6'>Next steps</span>
+                                </button>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-            </div>
+            </form>
         </div>
-      </form>
     )
 }
 
