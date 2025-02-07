2216d76e17592d9705332b0a72e1e9848b430e1f
eunkyeong <wjdtjr92@naver.com>
2025-02-07 20:04:56 +0900

    fix : itemsReducer 추가후 오류 수정중

--- entertainment-webapp/src/components/Movies.tsx
+++ entertainment-webapp/src/components/Movies.tsx
@@ -2,8 +2,9 @@ import { useState, useEffect } from 'react';
 import bookMarKOff from '../assets/icon-bookmark-empty.svg';
 import bookMarkOn from '../assets/icon-bookmark-full.svg';
 import { useSelector, useDispatch } from 'react-redux';
-import { setItem, toggle } from '../store/index';
-
+import { setItems, change } from '../store/index';
+import movie from '../assets/icon-category-movie.svg'
+import tv from '../assets/icon-category-tv.svg'
 
 interface Thumbnail {
   trending: {
@@ -33,11 +34,11 @@ export interface Category {
 }
 
 export const Movies = () => {
+
   const [ data , setData ] = useState<MovieSeries[]>([]);
+  const [filterData, setFilterData] = useState<MovieSeries[]>([]);
   const [ loading , setLoading ] = useState<boolean>(true);
   const [ error , setError ] = useState<string>();
-
-  const [filterData, setFilterData] = useState<MovieSeries[]>([]);
 
   const dispatch = useDispatch();
   const currentMenu = useSelector((state:any) => state.menuReducer);
@@ -59,10 +60,9 @@ export const Movies = () => {
           id: index + Math.random(),
         }));
 
-        dispatch(setItem(updatedData));
+        dispatch(setItems(updatedData));
         setData(updatedData);
-        console.log(currentItems);
-        setFilterData(updatedData);       
+        setFilterData(updatedData);
         setLoading(false);
       })
       .catch((error) => {
@@ -71,34 +71,32 @@ export const Movies = () => {
       });
     }, []);// 빈 배열을 넣어 컴포넌트 마운트 시 한 번만 실행
 
-
     //side bar 선택에 따라서 데이터 뿌려주기
     useEffect(() => {
-      const _data = data;
-      if(currentMenu == "bookmark"){
-        setFilterData(_data.filter((item:MovieSeries)=> item.isBookmarked));
-      }else if(currentMenu == "home"){
-        setFilterData(_data);
-      }else{
-        const filteredData : MovieSeries[] = _data.filter((item: MovieSeries) => (currentMenu == item.category?.toLowerCase().replace(/(\s*)/g, "")));
-        setFilterData(filteredData);
-      }
+        const _data = data;
+        if(currentMenu == "bookmark"){
+          setFilterData(_data.filter((item:MovieSeries)=> item.isBookmarked));
+        }else if(currentMenu == "home"){
+          setFilterData(_data);
+        }else{
+          const filteredData : MovieSeries[] = _data.filter((item: MovieSeries) => (currentMenu == item.category?.toLowerCase().replace(/(\s*)/g, "")));
+          setFilterData(filteredData);
+        }
     }, [currentMenu]);
 
     //검색어에 의해 데이터 뿌려주기
     useEffect(() => {
-      const _data = data;
-      setFilterData(_data.filter((item: MovieSeries)=> (item.title.toLowerCase()).indexOf(searchWord.toLowerCase()) > -1));
+        const _data = data;
+        setFilterData(_data.filter((item: MovieSeries)=> (item.title.toLowerCase()).indexOf(searchWord.toLowerCase()) > -1));
     }, [searchWord]);
     
 
    const handleToggleBookmark = (id : string) => {
-      dispatch(toggle(id)) ;
+      dispatch(change(id)) ;
     // const updateBookmark = data.map((item : MovieSeries) =>
     //   item.id == id ? { ...item, isBookmarked: !item.isBookmarked } : item
     // );
     // setData(updateBookmark);
-    console.log(currentItems);
    }
 
     if(error){
@@ -130,28 +128,24 @@ export const Movies = () => {
           <h1 className='title has-text-white'>
             { title }
           </h1>
-          <div className="columns is-multiline is-mobile is-2">
-              {currentItems!.map((item : MovieSeries) => {
+          <div className="columns pt-0 is-multiline is-mobile is-2">
+              {filterData!.map((item : MovieSeries) => {
                 return (
-                  <div 
-                    key={item.id} 
-                    className="column is-6-mobile is-4-tablet is-3-desktop is-relative"
-                  >
-                    <img className ="image" src={'http://localhost:5173/src/'+item.thumbnail.regular.small} />
-                    <div className='is-overlay has-text-centered'>
-                      <button 
-                        className="button is-dark is-rounded"
-                        onClick={()=>{ handleToggleBookmark(item.id)}}
-                      >
-                        <img src={item.isBookmarked ? bookMarkOn : bookMarKOff } />
-                      </button>
+                  <div key={item.id} className="column is-6-mobile is-4-tablet is-3-desktop is-relative">
+                    <div>
+                      <img className ="image is-radiusless m-1" src={'http://localhost:5173/src/'+item.thumbnail.regular.small} />
+                      <div className='has-text-right is-overlay'>
+                        <button className='mt-5 mr-4 is-rounded has-background-dark' type="button" onClick={()=>{ handleToggleBookmark(item.id)}}>
+                          <img className="image is-11x11" src={item.isBookmarked ? bookMarkOn : bookMarKOff } />
+                        </button>
+                      </div>
                     </div>
-                    <p>{item.year}</p>
-                    <p>{item.category}</p>
-                    <p>{item.rating}</p>
-                    <div className="item-text">
-                      <div className="title">{item.title}</div>
+                    <div className="columns is-0 mt-1 mb-0 has-text-white">
+                        <div className="column is-2">{item.year}</div>
+                        <div className="column is-4 has-text-centered"> <img src={item.category == 'Movie' ? movie : tv}/> {item.category}</div>
+                        <div className="column is-1">{item.rating}</div>
                     </div>
+                    <div className="title is-size-4 has-text-weight-medium has-text-white">{item.title}</div>
                   </div> 
                 )
               })}
