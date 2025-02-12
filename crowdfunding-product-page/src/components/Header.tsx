8189c53de2bd772d45d3b628e3f3ad4d0fce8b93
Bang04 <eunkyeong_b@naver.com>
2025-02-12 17:11:36 +0900

    feat :  메뉴 추가(블러처리 미포함)

--- crowdfunding-product-page/src/components/Header.tsx
+++ crowdfunding-product-page/src/components/Header.tsx
@@ -1,19 +1,36 @@
+import React, { useState } from "react";
+import "./Header.css";
+
 const Header = () => {
+  const [isOpenMenu , setIsOpenMenu ] = useState(false);
+  const menuOpen = () =>{
+    setIsOpenMenu(!isOpenMenu);
+  }
+
     return(
-        <nav className="navbar" role="navigation" aria-label="main navigation">
-        <div className="navbar-brand">
-          <a className="navbar-item" href="https://bulma.io">
-            corwdfund
-          </a>
-  
-          <a role="button" className="navbar-burger" aria-label="menu" aria-expanded="false">
+    <div className="header">
+      <div className="container is-flex is-align-items-center is-justify-content-space-between py-5">
+        <a className="is-size-5 has-text-weight-bold has-text-white">crowdfund</a>
+        <div className={ `navbar-burger has-text-white is-hidden-tablet is-hidden-desktop ${isOpenMenu ? "is-active" : ""}`} onClick={() => menuOpen()}>
             <span aria-hidden="true"></span>
             <span aria-hidden="true"></span>
             <span aria-hidden="true"></span>
             <span aria-hidden="true"></span>
-          </a>
+          </div>
+        <div className="is-flex is-hidden-mobile">
+          <div className="navbar-item is-size-7 has-text-white has-text-weight-medium mx-1">About</div>
+          <div className="navbar-item is-size-7 has-text-white has-text-weight-medium mx-1">Discover</div>
+          <div className="navbar-item is-size-7 has-text-white has-text-weight-medium mx-1">Get Started</div>
         </div>
-      </nav>
+      </div>
+
+      <div className={`navbar-menu mx-5  ${isOpenMenu ? "is-active" : ""} `}>
+          <div className="navbar-item is-size-6 has-text-black has-text-weight-medium pl-5 py-4">About</div>
+          <div className="navbar-item is-size-6 has-text-black has-text-weight-medium pl-5 py-3">Discover</div>
+          <div className="navbar-item is-size-6 has-text-black has-text-weight-medium pl-5 py-3">Get Started</div>
+      </div>
+      
+    </div>
     )
 };
 
